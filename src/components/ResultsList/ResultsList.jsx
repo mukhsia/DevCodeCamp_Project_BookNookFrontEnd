8a12b5d51c558e6478172be1e4c6eb505cfa7035
mukhsia <andre.n.mukhsia@gmail.com>
2024-01-01 23:54:59 -0800

    Book from search results Links to book Detail page with id as Route Param.

--- src/components/ResultsList/ResultsList.jsx
+++ src/components/ResultsList/ResultsList.jsx
@@ -1,14 +1,21 @@
 import React from 'react';
 import { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
 
 const ResultsList = ({ books }) => {
     return (
         <div className='container'>
             {console.log('ResultsList executing')}
-            {books &&
-                books.map((book) => {
-                    return <li key={book.id}>{book.volumeInfo.title}</li>;
-                })}
+            <ul>
+                {books &&
+                    books.map((book) => {
+                        return (
+                            <Link to={`/bookdetails/${book.id}`}>
+                                <li key={book.id}>{book.volumeInfo.title}</li>
+                            </Link>
+                        );
+                    })}
+            </ul>
         </div>
     );
 };
--- src/pages/BookDetailsPage/BookDetailsPage.jsx
+++ src/pages/BookDetailsPage/BookDetailsPage.jsx
@@ -3,13 +3,14 @@ import { useEffect, useState } from 'react';
 import useAuth from '../../hooks/useAuth';
 
 import axios from 'axios';
-import { useNavigate } from 'react-router-dom';
+import { useParams, useNavigate } from 'react-router-dom';
 
 const BookDetailsPage = () => {
     const [user, token] = useAuth();
     const navigate = useNavigate;
+    const { bookId } = useParams();
 
-    return <div></div>;
+    return <div>{bookId}</div>;
 };
 
 export default BookDetailsPage;
